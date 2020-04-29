b6ecaa0aa66885bf6d5275469a929150278d284a
wangjun <2209119121@qq.com>
2020-04-29 11:28:11 +0800

    测试一下

---
+++ app/src/main/java/com/yyzh/firsttestlib/TestUtli.java
@@ -0,0 +1,14 @@
+package com.yyzh.firsttestlib;
+
+import android.util.Log;
+
+/**
+ * Created by WJ
+ * Date: 2020/4/29 - 10:58
+ * des:
+ */
+public class TestUtli {
+    public static void log(String s){
+        Log.e("打印",s);
+    }
+}
